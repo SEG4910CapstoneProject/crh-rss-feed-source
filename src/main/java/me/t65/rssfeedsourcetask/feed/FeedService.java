7f0f726bc624b55c2205c7b7f6af21224db41242
Amani <alouendr@uottawa.ca>
2025-02-23 11:23:25 -0500

    pulled all articles from open cti as json

--- src/main/java/me/t65/rssfeedsourcetask/feed/FeedService.java
+++ src/main/java/me/t65/rssfeedsourcetask/feed/FeedService.java
@@ -1,6 +1,7 @@
 package me.t65.rssfeedsourcetask.feed;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /** Service that reads some data feed */
 public interface FeedService {
@@ -10,5 +11,10 @@ public interface FeedService {
      * @param feedUrlPair feed and source object
      * @return Flux containing stream of entries from feed
      */
-    Flux<ArticleData> getFeed(FeedUrlPair feedUrlPair);
+    //Flux<ArticleData> getFeed(FeedUrlPair feedUrlPair);
+
+
+
+
+    Mono<String> fetchArticlesFromOpenCti(String s);
 }
